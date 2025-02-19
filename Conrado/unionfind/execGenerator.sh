ff38ade021891ef50cea9d206f0e606db79e855d
Alex H <alexherrerobravo@gmail.com>
2025-02-19 13:29:17 +0100

    Fixed things

--- Conrado/unionfind/execGenerator.sh
+++ Conrado/unionfind/execGenerator.sh
@@ -1,13 +1,19 @@
 echo "Generating instances..."
+make clean
+make generator
+
+rm ./instances/*
 
 size=(1000 5000 10000 50000 100000)
 
 for n in ${size[@]}
 do
-    for seed in $(seq 1964 1984)
+    for seed in $(seq 1964 1983)
     do
         echo "Creating instance with size $n and seed $seed"
         output_file="./instances/size=${n}_seed=${seed}.txt"
         ./generator.exe $n $seed > $output_file
     done
 done
+
+make clean
---
+++ Conrado/unionfind/execMain.sh
@@ -0,0 +1,22 @@
+echo "Executing Quick Union with No Compression"
+make clean
+make main
+
+QU=0
+UW=1
+UR=2
+
+NC=0
+FC=1
+PS=2
+PH=3
+
+for f in $(ls ./instances)
+do
+    size=$(echo $f | grep -oP '(?<=size=)[0-9]+')
+    file="./instances/${f}"
+    (echo "$size $UR $PS" ; cat $file) | ./main.exe
+done
+
+echo "Finish!"
+make clean
